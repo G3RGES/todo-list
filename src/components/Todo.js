b59adcbd8f2b9ed578491ec51f75197914fd2fb7
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-05-18 15:22:47 +0300

    Update Todo.js

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,7 +1,26 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPenToSquare } from "@fortawesome/free-solid-svg-icons";
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
 
 const Todo = () => {
-  return <div>Todo</div>;
+  return (
+    <div className="Todo">
+      <p className="">Sleep</p>
+      <div className="">
+        <FontAwesomeIcon
+          className="edit-icon"
+          icon={faPenToSquare}
+          //   onClick={() => editTodo(task.id)}
+        />
+        <FontAwesomeIcon
+          className="delete-icon"
+          icon={faTrash}
+          //   onClick={() => deleteTodo(task.id)}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Todo;
