44fa0c985e11ceec3c5bcd953aaa3ea1d1e33de7
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-05-18 14:53:37 +0300

    Update TodoForm.js

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 
 const TodoForm = () => {
-  return <div>TodoForm</div>;
+  return (
+    <form className="TodoForm">
+      <input
+        type="text"
+        className="todo-input"
+        placeholder="what is the task today ?"
+      />
+      <button type="submit" className="todo-btn">
+        Add Task
+      </button>
+    </form>
+  );
 };
 
 export default TodoForm;
